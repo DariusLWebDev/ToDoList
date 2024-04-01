ceada2d2142912775ad6403b8d5e90667bdf81b1
DariusLWebDev <143473187+DariusLWebDev@users.noreply.github.com>
2024-04-01 21:42:44 +0800

    Set Up the To Do List

---
+++ To Do List/To Do List/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace To_Do_List
+{
+    partial class toDoList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.titleTextBox = new System.Windows.Forms.TextBox();
+            this.descriptionTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.newButton = new System.Windows.Forms.Button();
+            this.toDoListView = new System.Windows.Forms.DataGridView();
+            this.singleSearchQueryDefinitionBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.editButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.toDoListView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.singleSearchQueryDefinitionBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1840, 97);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "To Do List";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // titleTextBox
+            // 
+            this.titleTextBox.Location = new System.Drawing.Point(28, 145);
+            this.titleTextBox.Name = "titleTextBox";
+            this.titleTextBox.Size = new System.Drawing.Size(1817, 29);
+            this.titleTextBox.TabIndex = 1;
+            this.titleTextBox.TextChanged += new System.EventHandler(this.titleTextBox_TextChanged);
+            // 
+            // descriptionTextBox
+            // 
+            this.descriptionTextBox.Location = new System.Drawing.Point(28, 213);
+            this.descriptionTextBox.Name = "descriptionTextBox";
+            this.descriptionTextBox.Size = new System.Drawing.Size(1817, 29);
+            this.descriptionTextBox.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 181);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Description";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Title";
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(1418, 264);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(427, 48);
+            this.deleteButton.TabIndex = 7;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(961, 264);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(427, 48);
+            this.saveButton.TabIndex = 8;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // newButton
+            // 
+            this.newButton.Location = new System.Drawing.Point(28, 264);
+            this.newButton.Name = "newButton";
+            this.newButton.Size = new System.Drawing.Size(427, 48);
+            this.newButton.TabIndex = 9;
+            this.newButton.Text = "New";
+            this.newButton.UseVisualStyleBackColor = true;
+            this.newButton.Click += new System.EventHandler(this.newButton_Click);
+            // 
+            // toDoListView
+            // 
+            this.toDoListView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.toDoListView.BackgroundColor = System.Drawing.Color.White;
+            this.toDoListView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.toDoListView.Location = new System.Drawing.Point(28, 333);
+            this.toDoListView.Name = "toDoListView";
+            this.toDoListView.RowHeadersWidth = 72;
+            this.toDoListView.RowTemplate.Height = 31;
+            this.toDoListView.Size = new System.Drawing.Size(1817, 685);
+            this.toDoListView.TabIndex = 10;
+            // 
+            // singleSearchQueryDefinitionBindingSource
+            // 
+            this.singleSearchQueryDefinitionBindingSource.DataSource = typeof(MongoDB.Driver.Search.SingleSearchQueryDefinition);
+            // 
+            // editButton
+            // 
+            this.editButton.Location = new System.Drawing.Point(501, 264);
+            this.editButton.Name = "editButton";
+            this.editButton.Size = new System.Drawing.Size(427, 48);
+            this.editButton.TabIndex = 11;
+            this.editButton.Text = "Edit";
+            this.editButton.UseVisualStyleBackColor = true;
+            this.editButton.Click += new System.EventHandler(this.editButton_Click_1);
+            // 
+            // toDoList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(1874, 1058);
+            this.Controls.Add(this.editButton);
+            this.Controls.Add(this.toDoListView);
+            this.Controls.Add(this.newButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.descriptionTextBox);
+            this.Controls.Add(this.titleTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "toDoList";
+            this.Text = "To Do List";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.toDoListView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.singleSearchQueryDefinitionBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox titleTextBox;
+        private System.Windows.Forms.TextBox descriptionTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button newButton;
+        private System.Windows.Forms.DataGridView toDoListView;
+        private System.Windows.Forms.BindingSource singleSearchQueryDefinitionBindingSource;
+        private System.Windows.Forms.Button editButton;
+    }
+}
+
---
+++ To Do List/To Do List/Form1.cs
@@ -0,0 +1,167 @@
+﻿using Amazon.Runtime.Internal;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace To_Do_List
+{
+    public partial class toDoList : Form
+    {
+
+        public static IMongoClient client = new MongoClient("mongodb://localhost:27017");
+        public static IMongoDatabase db = client.GetDatabase("ToDoList");
+        public static IMongoCollection<Entry> coll = db.GetCollection<Entry>("mycoll");
+
+        public toDoList()
+        {
+            InitializeComponent();
+        }
+
+        DataTable todoList = new DataTable();
+        bool isEditing = false;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Create Columns
+            todoList.Columns.Add("Title");
+            todoList.Columns.Add("Description");
+
+            //Point our datagridview to our datasource
+            toDoListView.DataSource = todoList;
+
+        }
+
+        private void newButton_Click(object sender, EventArgs e)
+        {
+            // Add the new task to the DataTable
+            todoList.Rows.Add(titleTextBox.Text, descriptionTextBox.Text);
+
+            // Clear fields
+            titleTextBox.Text = "";
+            descriptionTextBox.Text = "";
+
+            // Update the MongoDB collection with the changes
+            UpdateMongoDB();
+
+        }
+
+        private void editButton_Click_1(object sender, EventArgs e)
+        {
+            if (toDoListView.SelectedRows.Count > 0)
+            {
+                // Get the index of the selected row
+                int rowIndex = toDoListView.SelectedRows[0].Index;
+
+                // Populate the text boxes with the data from the selected row for editing
+                titleTextBox.Text = todoList.Rows[rowIndex]["Title"].ToString();
+                descriptionTextBox.Text = todoList.Rows[rowIndex]["Description"].ToString();
+
+                // Set the editing flag to true
+                isEditing = true;
+            }
+            else
+            {
+                MessageBox.Show("Please select a task to edit.");
+            }
+
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            if (isEditing)
+            {
+                // Get the index of the selected row
+                int rowIndex = toDoListView.SelectedRows[0].Index;
+
+                // Update the current row in the DataTable
+                todoList.Rows[rowIndex]["Title"] = titleTextBox.Text;
+                todoList.Rows[rowIndex]["Description"] = descriptionTextBox.Text;
+
+                // Clear fields
+                titleTextBox.Text = "";
+                descriptionTextBox.Text = "";
+
+                // Reset editing flag
+                isEditing = false;
+
+                // Update the MongoDB collection with the changes
+                UpdateMongoDB();
+            }
+            else
+            {
+                MessageBox.Show("Please select a task to edit.");
+            }
+
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            if (toDoListView.SelectedRows.Count > 0)
+            {
+                int rowIndex = toDoListView.SelectedRows[0].Index;
+                todoList.Rows.RemoveAt(rowIndex); // Remove the selected row from the DataTable
+                UpdateMongoDB(); // Update MongoDB after deleting the row
+            }
+            else
+            {
+                MessageBox.Show("Please select a task to delete.");
+            }
+
+        }
+
+        
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        public class Entry
+        {
+            [BsonId]
+
+            public ObjectId Id { get; set; }
+
+            [BsonElement("titleTextBox")]
+
+            public string titleTextBox { get; set; }
+
+            [BsonElement("descriptionTextBox")]
+
+            public string descriptionTextBox { get; set; }
+
+            public Entry(string titleTextBox, string descriptionTextBox)
+            {
+                this.titleTextBox = titleTextBox;
+                this.descriptionTextBox = descriptionTextBox;
+            }
+        }
+
+        private void UpdateMongoDB()
+        {
+            // Clear the MongoDB collection
+            coll.DeleteMany(new BsonDocument());
+
+            // Insert each row from the DataTable into the MongoDB collection
+            foreach (DataRow row in todoList.Rows)
+            {
+                Entry user = new Entry(row["Title"].ToString(), row["Description"].ToString());
+                coll.InsertOne(user);
+            }
+        }
+        private void titleTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
